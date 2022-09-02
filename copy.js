4505bf46ededaf209052fd9d662595320d6a8198
harumascaremath <111788205+harumascaremath@users.noreply.github.com>
2022-09-02 13:44:30 +0700

    Add files via upload

---
+++ copy.js
@@ -0,0 +1,10 @@
+function copyText(){
+
+ document.getElementById("sunflower").select();
+ document.execCommand('copy');
+}
+function copyText(){
+
+ document.getElementById("peashooter").select();
+ document.execCommand('copy');
+}
---
+++ copy_button.js
@@ -0,0 +1,14 @@
+var $ = mdui.$;
+
+$('#copy_sunflower').on('click', function () {
+  mdui.snackbar({
+    message: 'Copied',
+    position: 'left-top'
+  });
+});
+$('#copy_peashooter').on('click', function () {
+  mdui.snackbar({
+    message: 'Copied',
+    position: 'left-top'
+  });
+});
