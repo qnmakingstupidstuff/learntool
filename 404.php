37a7add5e648b0d495895ca31b2be12f2b61b7f5
harumascaremath <111788205+harumascaremath@users.noreply.github.com>
2022-09-06 03:41:40 +0700

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php
+
+    header("HTTP/1.0 404 Not Found");
+
+    echo '<html>
+            <head>
+                <meta http-equiv="Refresh" content="0;url=https://harumatool.fun/" />
+            </head><body></body>
+          </html>';
+
+
+?>
