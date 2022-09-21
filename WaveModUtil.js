199bf92231d63873f0b0405befbb22161ec0854a
Quang Nguyen <111788205+qnmakingstupidstuff@users.noreply.github.com>
2022-09-22 02:46:57 +0700

    Add files via upload
    Signed-off-by: Quang Nguyen <111788205+qnmakingstupidstuff@users.noreply.github.com>

--- WaveModUtil.js
+++ WaveModUtil.js
@@ -2,6 +2,7 @@ function loadRipeData(){
     const div_disable = document.getElementById("menu-bar");
     const contain_mod = document.getElementById("contain-data");
     const mod_open = document.getElementById("mod-hide");
+    const server = document.getElementById("customServer").value;
     var bar  = '';
     mod_open.style.display = "block";
     div_disable.style.display = "none";
@@ -10,7 +11,7 @@ function loadRipeData(){
     const key_length = parseInt(dataArray.length);
     const missing_key ="'missing.png'";
     for(let i = 0;i<key_length;i++){
-        bar += '<img src="./HarumaTools/zombies/'+dataArray[i]+'.png" draggable="false" id="'+dataArray[i]+'" onclick="selectSprite(event);placeFunction()" />';
+        bar += '<img src="'+server+''+dataArray[i]+'.png" draggable="false" id="'+dataArray[i]+'" onclick="selectSprite(event);placeFunction()" />';
     }
     contain_mod.innerHTML = bar;
 }
