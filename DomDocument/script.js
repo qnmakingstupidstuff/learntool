208642dccee71740705937dc619684293f1857c6
Quang Nguyen <111788205+qnmakingstupidstuff@users.noreply.github.com>
2022-10-06 22:10:32 +0700

    Add files via upload
    Signed-off-by: Quang Nguyen <111788205+qnmakingstupidstuff@users.noreply.github.com>

--- DomDocument/script.js
+++ DomDocument/script.js
@@ -1,3 +1,24 @@
+var count_source = 0;
+var count_image = 0;
+var count_labels_core = 0;
+var count_labels_total = 0;
+var count_actions_core = 0;
+var isSPC = null;
+var audio_shell = 0;
+var isTwinKles = null;
+var storage_m = [];
+var storage_a = [];
+var storage_labels_name = [];
+var storage_labels_index = [];
+var storage_labels_duration = [];
+var storage_action_index = [];
+var storage_action_duration = [];
+var storage_action_trigger = [];
+var storage_audio_index = [];
+var storage_audio_duration = [];
+var storage_audio_call = [];
+var storage_images = [];
+var fps_core = -1;
 function domRead(){
     //This is where the DOM Build
     const documentDom = document.getElementById("documentDom").value;
@@ -65,6 +86,7 @@ function domRead(){
     var take2 = take1.replaceAll('"','');
     fps_count = take2;
     document.getElementById("fps_edit").value = fps_count;
+    fps_core = fps_count;
     var isSPCUtil = null;
     if (spcutil != -1){
         isSPCUtil = true;
@@ -72,20 +94,31 @@ function domRead(){
     else{
         isSPCUtil = false;
     }
+    //Giving variables
+    if (isSPCUtil = true){
+        isSPC = true
+    }
+    else if (isSPCUtil = false){
+        isSPC = false
+    }
+    else{
+        isSPC = null
+    }
     var count_bit = 0;
     var split_core = [];
     //Checking core
-    for (let i = bit_item[0];i<count_bititem;i++){
-        var changed_bititem = document_split[i].replaceAll('name="','');
-        var core_item = changed_bititem.replaceAll('"','');
-        var split_shell = core_item.split(" ");
-        if (split_shell.length <= 4){
-            split_core[count_bit] += split_shell[1]
+    for (let i = bit_item[0];i<bit_item[0]+count_bititem;i++){
+        var changed_bititem = document_split[i].replaceAll('<DOMBitmapItem name="','');
+        var core_item = changed_bititem.replaceAll('" href="',' ');
+        var shell_item = core_item.replaceAll(' />','');
+        var split_shell = shell_item.split(" ");
+        if (split_shell.length == 2){
+            split_core[count_bit] += split_shell[0];
+            count_bit++;
         }
-        count_bit++;
     }
     for (let i = 0;i<split_core.length;i++){
-        split_core[i] = split_core[i].replaceAll("undefined", "")
+        split_core[i] = split_core[i].replaceAll("undefined", "");
     }
     //Making M_Elements
     if (count_Melement != 0){
@@ -101,6 +134,7 @@ function domRead(){
             split_m[i] = split_m[i].replaceAll("undefined", "")
         }
     }
+    count_source = count_m;
     //count A_Elements
     var push_A = [];
     var count_a = 0;
@@ -111,6 +145,7 @@ function domRead(){
             count_a++;
         }
     }
+    count_image = count_a;
     //Finding A
     if(count_a != 0){
         var count_value = 0;
@@ -137,6 +172,7 @@ function domRead(){
         input.style.height = "auto";
         input.style.fontSize = "15px";
         picture_util.appendChild(input);
+        storage_images[i] = split_core[i];
     }
     //Building M_Elements
     var m_util = document.getElementById("m_util");
@@ -150,6 +186,8 @@ function domRead(){
         input.style.height = "auto";
         input.style.fontSize = "15px";
         m_util.appendChild(input);
+        //Let M become JSON
+        storage_m[i] = split_m[i];
     }
     //Building A_Elements
     var a_util = document.getElementById("a_util");
@@ -163,6 +201,8 @@ function domRead(){
         input.style.height = "auto";
         input.style.fontSize = "15px";
         a_util.appendChild(input);
+        //Let A become JSON
+        storage_a[i] = split_a[i];
     }
     //Build labels core
     var first_label = -1;
@@ -189,7 +229,7 @@ function domRead(){
         labels[count_label] = document_split[i];
         count_label++;
     }
-    //Counting domframe
+    //Finding first domframe
     var count_domframe = 0;
     var first_domframe = -1;
     for (let i = 0; i < labels.length; i++){
@@ -206,12 +246,14 @@ function domRead(){
             count_domframe++;
         }
     }
+    //Animation count
     var animation_labels = [];
     var count_animation = 0;
     for (let i = first_domframe; i < (first_domframe+count_domframe); i++){
         animation_labels[count_animation] = labels[i];
         count_animation++;
     }
+    count_labels_core = count_animation;
     //Build labels
     var core_labels = [];
     var core_index = [];
@@ -272,6 +314,10 @@ function domRead(){
         label_util.appendChild(label_total);
         var spacebar = document.createElement('br');
         label_util.appendChild(spacebar);
+        //Porting Labels to JSON
+        storage_labels_name[i] = core_name[i];
+        storage_labels_index[i] = parseInt(core_index[i]);
+        storage_labels_duration[i] = parseInt(core_duration[i]);
     }
     //Cloning core
     var core_data = [];
@@ -365,7 +411,8 @@ function domRead(){
             count_eventlist++
         } 
     }
-    console.log(count_eventlist)
+    //Counting
+    count_actions_core = count_eventlist;
     //Putting to the tab
     const anim_action = document.getElementById('anim_action');
     anim_action.style.display = "block";
@@ -408,5 +455,243 @@ function domRead(){
         anim_action.appendChild(action_trigger_event);
         var spacebar = document.createElement('br');
         anim_action.appendChild(spacebar);
+        //Porting Action
+        storage_action_index[i] = parseInt(action_index_main[i]);
+        storage_action_duration[i] = parseInt(action_duration_main[i]);
+        storage_action_trigger[i] = (events_list[i]);
     }
+    //PvZ2 Soundbank
+    //Find audio
+    var first_audio = -1;
+    var second_audio = -1;
+    for(let i = 0; i < document_split.length; i++){
+        var find_audio = document_split[i].search('<DOMLayer name="audio">');
+        if(find_audio != -1){
+            first_audio = i;
+            break
+        }
+    }
+    //Copying the list
+    var cloning_arrays = [];
+    var soundbank_count = 0;
+    for (let i = first_audio; i < document_split.length; i++){
+        cloning_arrays[soundbank_count] = document_split[i];
+        soundbank_count++;
+    }
+    //Find audio
+    for(let i = 0; i < cloning_arrays.length; i++){
+        var find_audio = cloning_arrays[i].search('</DOMLayer>');
+        if(find_audio != -1){
+            second_audio = i;
+            break
+        }
+    }
+    //Find first audio
+    for(let i = 0; i < cloning_arrays.length; i++){
+        var find_audio = cloning_arrays[i].search('<DOMLayer name="audio">');
+        if(find_audio != -1){
+            first_audio = i;
+            break
+        }
+    }
+    //Soundbank List
+    var soundbank_list = [];
+    var soundbank_pool = 0;
+    for (let i = first_audio; i < second_audio; i++){
+        soundbank_list[soundbank_pool] = cloning_arrays[i];
+        soundbank_pool++;
+    }
+    //Handle Soundbank
+    var audio_index_list = [];
+    var audio_duration_list = [];
+    var audio_total_list = [];
+    var audio_count = 0;
+    for(let i = 0; i < soundbank_list.length; i++){
+        var find_domframe = soundbank_list[i].search("DOMFrame");
+        var find_duration = soundbank_list[i].search("duration");
+        if(find_domframe != -1){
+            if(find_duration != -1){
+                var domShell = soundbank_list[i].replaceAll('<DOMFrame index="','');
+                var domShell01 = domShell.replaceAll('" duration="',' ');
+                var domShell02 = domShell01.replaceAll('" />','');
+                var domEpic = domShell02.split(' ');
+                if (domEpic.length == 2){
+                    audio_index_list[audio_count] = domEpic[0];
+                    audio_duration_list[audio_count] = domEpic[1];
+                    audio_total_list[audio_count] = parseInt(audio_index_list[audio_count]) + parseInt(audio_duration_list[audio_count]);
+                    audio_count++;
+                }
+            }
+        }
+    }
+    audio_shell = audio_count;
+    //Soundbank Done
+    //Handle Event Soundbank
+    var audio_event = [];
+    var audio_event_count = 0;
+    for (let i = 0; i < soundbank_list.length; i++){
+        var find_domframe = soundbank_list[i].search("CDATA");
+        if(find_domframe != -1){
+            var event_node = soundbank_list[i].replaceAll('<![CDATA[fscommand("', '');
+            var event_node01 = event_node.replaceAll('", "', ' ');
+            var event_node02 = event_node01.replaceAll('");]]>', '');
+            var current_audio = event_node02.split(' ');
+            if (current_audio.length == 2){
+                audio_event[audio_event_count] = current_audio[1];
+                audio_event_count++;
+            }
+        }
+    }
+    //Porting to Web
+    var anim_audio = document.getElementById('anim_audio');
+    anim_audio.style.display = "block";
+    //Port JSON
+    for(let i = 0; i < audio_count; i++){
+        storage_audio_index[i] = parseInt(audio_index_list[i]);
+        storage_audio_duration[i] = parseInt(audio_duration_list[i]);
+    }
+    for (let i = 0; i < soundbank_list.length; i++){
+        storage_audio_call[i] = audio_event[i];
+    }
+}
+function DomtoJSON(){
+    var duration_count = 0;
+    for (let i = 0; i < storage_labels_duration.length; i++){
+        duration_count += parseInt(storage_labels_duration[i])
+    }
+    var output = '{\n    "$comment": "This JSON file was converted by Haruma Tool, DomDocument to JSON",\n';
+    if (fps_core != -1){
+        output += '    "fps-build": '+parseInt(fps_core)+',\n';
+    }
+    output += '	"images": ['
+    if (storage_a.length != 0){
+        output += '\n';
+        for(let i = 0; i < storage_images.length; i++){
+            if(i == (storage_images.length-1)){
+                output += '        "'+storage_images[i]+'"\n';
+                output += '    ],\n'
+            }
+            else{
+                output += '        "'+storage_images[i]+'",\n';
+            }
+        }
+    }
+    else{
+        output += '],\n'
+    }
+    output += '	"M_Database": ['
+    if (storage_m.length != 0){
+        output += '\n';
+        for(let i = 0; i < storage_m.length; i++){
+            if(i == (storage_m.length-1)){
+                output += '        "'+storage_m[i]+'"\n';
+                output += '    ],\n'
+            }
+            else{
+                output += '        "'+storage_m[i]+'",\n';
+            }
+        }
+    }
+    else{
+        output += '],\n'
+    }
+    output += '	"A_Database": ['
+    if (storage_a.length != 0){
+        output += '\n';
+        for(let i = 0; i < storage_a.length; i++){
+            if(i == (storage_a.length-1)){
+                output += '        "'+storage_a[i]+'"\n';
+                output += '    ],\n'
+            }
+            else{
+                output += '        "'+storage_a[i]+'",\n';
+            }
+        }
+    }
+    else{
+        output += '],\n'
+    }
+    if (isSPC = true){
+        output += '    "isSPCUtil": true,\n';
+    }
+    else{
+        output += '    "isSPCUtil": false,\n';
+    }
+    output += '    "frameCount": '+duration_count+',\n';
+    output += '    "Labels": [\n';
+    for (let i = 0; i < storage_labels_name.length; i++){
+        if(i == (storage_labels_name.length - 1)){
+            output += '        {\n';
+            output += '           "name": "'+storage_labels_name[i]+'",\n';
+            output += '           "start": '+storage_labels_index[i]+',\n';
+            output += '           "duration": '+storage_labels_duration[i]+'\n';
+            output += '        }\n';
+        }
+        else{
+            output += '        {\n';
+            output += '           "name": "'+storage_labels_name[i]+'",\n';
+            output += '           "start": '+storage_labels_index[i]+',\n';
+            output += '           "duration": '+storage_labels_duration[i]+'\n';
+            output += '        },\n';
+        }
+    }
+    output += '    ],\n';
+    output += '    "actions": [\n';
+    //Make action arrays
+    for (let i = 0; i < storage_action_index.length; i++){
+        if(i == (storage_action_index.length - 1)){
+            output += '        {\n';
+            output += '           "start": '+storage_action_index[i]+',\n';
+            output += '           "duration": '+storage_action_duration[i]+',\n';
+            output += '           "action": "'+storage_action_trigger[i]+'"\n';
+            output += '        }\n';
+        }
+        else{
+            output += '        {\n';
+            output += '           "start": '+storage_action_index[i]+',\n';
+            output += '           "duration": '+storage_action_duration[i]+',\n';
+            output += '           "action": "'+storage_action_trigger[i]+'"\n';
+            output += '        },\n';
+        }
+    }
+    output += '    ],\n';
+    //Shell Audio
+    output += '    "audio": [';
+    if(storage_audio_index.length != 0){
+        output += '\n';
+        for (let i = 0; i < storage_audio_index.length; i++){
+            if(i == (storage_audio_index.length - 1)){
+                output += '        {\n';
+                output += '           "start": '+storage_audio_index[i]+',\n';
+                output += '           "duration": '+storage_audio_duration[i]+',\n';
+                output += '           "action": "'+storage_audio_call[i]+'"\n';
+                output += '        }\n';
+                output += '    ]\n';
+            }
+            else{
+                output += '        {\n';
+                output += '           "start": '+storage_audio_index[i]+',\n';
+                output += '           "duration": '+storage_audio_duration[i]+',\n';
+                output += '           "action": "'+storage_audio_call[i]+'"\n';
+                output += '        },\n';
+            }
+        }
+    }
+    else{
+        output += ']\n';
+    }
+    //Core Output
+    output += '}'
+    var json_name = document.getElementById("json_name").value;
+    var text = output;
+    text = text.replace(/\n/g, "\r\n");
+    var blob = new Blob([text], { type: "application/json" });
+    var anchor = document.createElement("a");
+    anchor.download = json_name;
+    anchor.href = window.URL.createObjectURL(blob);
+    anchor.target = "_blank";
+    anchor.style.display = "none";
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
 }
